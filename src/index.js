d9f83a4c5751b245b4f0948454a96c2c9ecbfe40
mom0tomo <momo76w@gmail.com>
2018-12-31 19:28:56 +0900

    Section2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+	return 'Click on me!';
+}
+
 const App = () => {
-	return <div>Hi There!</div>;
+	return (
+		<div>
+			<label className="label" for="name">
+				Enter Name:
+			</label>
+			<input id="name" type="text" />
+			<button style={{ backgroundColor: 'blue', color: 'white' }}>{getButtonText()}</button>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
